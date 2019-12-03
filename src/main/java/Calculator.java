16103ddc802dd7dbbbe72b46d99aa6e05bead9c8
Dawid Koziej <januszakahipis@wp.pl>
2019-12-03 12:52:00 +0100

    add find class method and constructor

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,27 @@
+import org.apache.commons.io.FilenameUtils;
+
+
+import java.io.File;
+
 public class Calculator {
+    private String pluginsPath;
+    public Calculator(String pluginsPath){
+        this.pluginsPath = pluginsPath;
+    }
+    private void findClassFiles(){
+        File dir = new File(pluginsPath);
+        try {
+            for(File fileEntry : dir.listFiles()){
+                if(FilenameUtils.getExtension(fileEntry.getName()).equals("class"))
+                {
+                    System.out.println("mamy class");
+                }else {
+                    System.out.println("nie mamy class");
+                }
+                //System.out.println(fileEntry.getName());
+        }
+        } catch (NullPointerException e) {
+            System.out.println("Folder jest pusty");
+        }
+    }
 }
