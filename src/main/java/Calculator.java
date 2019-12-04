f228fc0df2450763eeecbcb74a896d0c5537b2d6
Dawid Koziej <januszakahipis@wp.pl>
2019-12-04 22:43:46 +0100

    add start method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,5 @@
 import org.apache.commons.io.FilenameUtils;
+import sun.font.TrueTypeFont;
 
 
 import java.io.File;
@@ -7,17 +8,19 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Calculator {
     private String pluginsPath;
     private List<ArithmeticOperation> listOfReadClass = new ArrayList <ArithmeticOperation>();
     Calculator(String pluginsPath){
         this.pluginsPath = pluginsPath;
+        findClassFiles();
     }
-    public List<ArithmeticOperation> getListOfReadClass(){
+    List<ArithmeticOperation> getListOfReadClass(){
         return listOfReadClass;
     }
-    void findClassFiles(){
+    private void findClassFiles(){
         File dir = new File(pluginsPath);
         try {
             for(File fileEntry : dir.listFiles()){
@@ -30,6 +33,7 @@ class Calculator {
                 }
                 System.out.println(fileEntry.getName());
         }
+            System.out.println("Wczytywanie zakończone powodzeniem");
         } catch (NullPointerException e) {
             System.out.println("Folder jest pusty");
         } catch (IllegalAccessException e) {
@@ -39,22 +43,39 @@ class Calculator {
         }
     }
     private void loadClass(String nameClass) throws IllegalAccessException, InstantiationException {
-        // Create a File object on the root of the directory containing the class file
         File file = new File(pluginsPath);
         try {
-            // Convert File to a URL
             URL url = file.toURI().toURL();
             URL[] urls = new URL[]{url};
 
-            // Create a new class loader with the directory
             ClassLoader cl = new URLClassLoader(urls);
 
-            // Load in the class; MyClass.class should be located in
-            // the directory file:/c:/myclasses/com/mycompany
             Class cls = cl.loadClass(nameClass);
             listOfReadClass.add((ArithmeticOperation) cls.newInstance());
         } catch (MalformedURLException e) {
         } catch (ClassNotFoundException e) {
+        }
+    }
+    void start(){
+        while(true) {
+            int i = 0;
+            for (; i < listOfReadClass.size(); i++) {
+                System.out.printf("%d. %s\n", i, listOfReadClass.get(i).getClass().getName());
+            }
+            System.out.printf("%d. %s\n", i + 1, "Exit");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj a:");
+            float a = scanner.nextFloat();
+            System.out.println("Podaj b:");
+            float b = scanner.nextFloat();
+            System.out.println("Podaj operacje");
+            int operacja = scanner.nextInt();
+            if (operacja < listOfReadClass.size() && operacja >= 0) {
+                System.out.println(listOfReadClass.get(operacja).function(a, b));
+            }
+            else if(operacja == i + 1){
+                break;
+            }
         }
     }
 }
