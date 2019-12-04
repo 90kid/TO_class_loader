26afd1bbdea3a519d4a64a45d2f5eb1305643631
Dawid Koziej <januszakahipis@wp.pl>
2019-12-04 22:44:28 +0100

    call calculator.start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator("src/main/plugins/");
-        calculator.findClassFiles();
-        System.out.println(calculator.getListOfReadClass().get(0).function(1.f, 2.f));
-        System.out.println(calculator.getListOfReadClass().get(1).function(1.f, 2.f));
-        System.out.println(calculator.getListOfReadClass().get(2).function(1.f, 2.f));
-        System.out.println(calculator.getListOfReadClass().get(3).function(1.f, 2.f));
+        calculator.start();
     }
 }
